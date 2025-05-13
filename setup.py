264b7ed2d8b8a18999f6b7631afd44e02f820fbb
abu-muhammedx7 <realmuhammed70@gmail.com>
2025-05-13 13:54:08 +0100

    Changes in Code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='medbot-healthcare',
     version='1.0.0',
     description='A Flask-based Medical Question Answering Bot',
-    author='CALEB UNIVERSITY BUESA REPRESENTATIVe',
+    author='CALEB UNIVERSITY BUESA REPRESENTATIVE',
     url='https://github.com/abu-muhammedx7/medbot-healthcare',
     packages=find_packages(),
     include_package_data=True,  # includes static, templates, etc.
